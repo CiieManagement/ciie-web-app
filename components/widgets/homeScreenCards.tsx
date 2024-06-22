8b4379302ad029614e575b5e798bdfafc5b5901f
dark-matter-007 <ashwin.aksharma.p@outlook.com>
2024-06-22 15:32:58 +0530

    fix searching card buttons not having proper margin from top

--- components/widgets/homeScreenCards.tsx
+++ components/widgets/homeScreenCards.tsx
@@ -16,14 +16,14 @@ function SearchingForInnovationCard() {
                     <Link href="/connect">
                         <button className='mt-10 w-fit rounded-full py-2 mx-auto shadow-sm text-white font-bold hover:scale-110 transition-all duration-300 px-4 bg-gradient-to-l from-purple-500 to-blue-400'>Join and Innovate with us</button>
                     </Link>
-                    <button className='mt-4 w-fit rounded-full py-2 mx-auto shadow-sm text-white font-bold px-4 bg-gradient-to-l hover:scale-110 transition-all duration-300 from-purple-500 to-blue-400'> Or independently contribute your ideas</button>
+                    <button className='mt-2 w-fit rounded-full py-2 mx-auto shadow-sm text-white font-bold px-4 bg-gradient-to-l hover:scale-110 transition-all duration-300 from-purple-500 to-blue-400'> Or independently contribute your ideas</button>
                 </div>
             </div>
             <div className='flex-row w-full h-fit mx-auto text-center rounded-2xl bg-gray-400/20 py-2 px-10 lg:pt-10 lg:px-10 pb-0 mt-10 lg:mt-20 z-20 transition-all duration-500 backdrop-blur-sm lg:flex hidden animate-appearance-in '>
                 <div className='flex flex-col max-w-lg'>
                     <h1 className='text-3xl lg:text-4xl font-bold text-[#8800ff]'> Searching for Innovation </h1>
                     <h1 className='mt-2 text-lg'>CIIE Provides the direction for the innovative ideas and creators</h1>
-                    <h1 className='mt-2 text-lg'>it&apos;s about igniting the passion and purpose within every individual, guiding them towards a future where innovation thrives and possibilities are endless.</h1>
+                    <h1 className='mt-2 text-lg mb-auto'>it&apos;s about igniting the passion and purpose within every individual, guiding them towards a future where innovation thrives and possibilities are endless.</h1>
                     <Link href="/connect">
                         <button className='mt-auto mb-2 w-fit rounded-full py-2 mx-auto shadow-sm text-white font-bold hover:scale-110 transition-all duration-300 px-4 bg-gradient-to-l from-purple-500 to-blue-400'> Join and Innovate with us</button>
                     </Link>
