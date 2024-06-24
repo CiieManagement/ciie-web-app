5b0f22b19fe533bc746f960ad4591aeb9dacd0ae
Shivansh Shukla <prasantshukla89@gmail.com>
2024-06-24 12:01:04 +0530

    Corrected dates in summart training program cards

--- public/values/strings.tsx
+++ public/values/strings.tsx
@@ -207,8 +207,8 @@ const Strings = {
           description:
             "In this workshop, we dive deep into app development technologies like Flutter and React-Native that are emerging nowadays.",
           registration_date: "2024-05-18T10:00:00",
-          start_date: "20 May 2024",
-          end_date: "22 May 2024",
+          start_date: "2 july 2024",
+          end_date: "30 july 2024",
           link:"https://omnidev-courses.vercel.app/app"
         },
         2: {
@@ -216,8 +216,8 @@ const Strings = {
           image: "/cloud.jpg",
           description: "In this workshop, we dive deep into cloud technologies that are emerging nowadays",
           registration_date: "2024-05-18T12:00:00",
-          start_date: "22 May 2024",
-          end_date: "24 May 2024",
+          start_date: "2 july 2024",
+          end_date: "30 july 2024",
           link:"https://omnidev-courses.vercel.app/cloud"
         },
         3: {
@@ -225,8 +225,8 @@ const Strings = {
           image: "/web.jpg",
           description: "In this workshop, we dive deep into web development technologies that are emerging nowadays",
           registration_date: "2024-05-18T14:00:00",
-          start_date: "25 May 2024",
-          end_date: "27 May 2024",
+          start_date: "25 july 2024",
+          end_date: "30 july 2024",
           link: "https://omnidev-courses.vercel.app/web"
         },
         4: {
@@ -234,8 +234,8 @@ const Strings = {
           image: "/dsa.jpg",
           description: "In this workshop, we dive deep into data structures that are emerging nowadays",
           registration_date: "2024-05-18T16:00:00",
-          start_date: "28 May 2024",
-          end_date: "30 May 2024",
+          start_date: "2 july 2024",
+          end_date: "30 july 2024",
           link:"https://omnidev-courses.vercel.app/dsa"
         },
       },
