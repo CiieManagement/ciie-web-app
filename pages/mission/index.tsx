33057c0dd6cabca90ca4e027bc08ffb44d606588
Shivansh Shukla <prasantshukla89@gmail.com>
2025-01-17 17:06:25 +0530

    testing

--- pages/mission/index.tsx
+++ pages/mission/index.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
-import { VisionCard } from '@/components/widgets/aboutScreenCards'
-import { Strings } from '@/public/values/strings'
-  
+// import { VisionCard } from '@/components/widgets/aboutScreenCards'
+// import { Strings } from '@/public/values/strings'
+// import { Button } from '@nextui-org/react'
+// import Link from 'next/link'
+// import { IoMdExit } from 'react-icons/io'
+import DefaultLayout from '@/layouts/default'
+
 const Index = () => {
   return (
-     <div className="relative min-h-screen flex items-center justify-center py-12">
+    <DefaultLayout>
+      <div >nsnsdk</div>
+    {/* <div className="relative min-h-screen flex items-center justify-center py-12">
       <div className="relative z-10 flex flex-col items-center text-center px-4 py-8 bg-opacity-80 rounded-lg max-w-6xl">
         <h1 className="mt-4 text-left text-3xl font-bold mb-10 md:text-4xl">
           Our Mission
@@ -20,9 +26,9 @@ const Index = () => {
           ))}
         </div>
       </div>
-    </div>
-  
-   )
+    </div> */}
+    </DefaultLayout>
+  )
 }
 
 export default Index
